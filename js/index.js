41dd9a01d4b36e1527066c1c6970bbe3d70adff5
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-06-30 17:20:30 -0300

    Add events for my main page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Script for my Portfolio
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    
+  })
+}
