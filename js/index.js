d325f810596b019b98d605f0495b45231629bb21
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-07-01 17:49:02 -0300

    Add function for remove menu

--- js/index.js
+++ js/index.js
@@ -16,3 +16,13 @@ if (navClose) {
     navMenu.classList.remove('show_menu')
   })
 }
+
+// Remove menu Mobile 
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each "nav_link", we remove the show-menu class
+    navMenu.classList.remove('show_menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
