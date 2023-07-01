df22027b5d260ff142a373fa2fa1cb01ee42bac5
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-07-01 16:21:32 -0300

    Add show and hide menu mobile

--- js/index.js
+++ js/index.js
@@ -3,8 +3,16 @@ const navMenu = document.getElementById('nav-menu')
 const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 
+// Show menu Mobile
 if (navToggle) {
   navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
+    navMenu.classList.add('show_menu')
+  })
+}
+
+// Hide menu Mobile
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show_menu')
   })
 }
