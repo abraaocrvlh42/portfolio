102d576b96ec39a44b60726d2061272c9a1009a2
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-10-17 13:09:33 -0300

    Script for portfolio

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Script for my Portfolio
+
+const elementProjects = document.getElementById('project__content')
+
+const loadProjects = (projects) => {
+  projects.forEach(project => {
+    const elementProjects = document.createElement('a')
+    // Get Attributes
+    elementProjects.setAttribute('href', project.link)
+    elementProjects.setAttribute('target', '_blank')
+
+    // Get Classes
+    elementProjects.classList.add('project')
+
+    // Get Images
+    
+  })
+
+  console.log(elementProjects)
+}
+
+fetch('./projects.json').then(response => response.json()).then(loadProjects)
