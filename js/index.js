3069fbb019ef01d4f86ccea16456656bb1910bca
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2023-10-17 15:11:49 -0300

    Script for portfolio

--- js/index.js
+++ js/index.js
@@ -1,37 +1,69 @@
 // Script for my Portfolio
 
-const elementProjects = document.getElementById('project__content')
+const elemProjects = document.getElementById('project__content')
 
-const loadProjects = (projects) => {
-  projects.forEach(project => {
-    const elementProjects = document.createElement('a')
-    // Get Attributes
-    elementProjects.setAttribute('href', project.link)
-    elementProjects.setAttribute('target', '_blank')
+const createImage = (projectImage, projectName) => {
+  const elemPicture = document.createElement('picture')
+  const elemImg = document.createElement('img')
 
-    // Get Classes
-    elementProjects.classList.add('project')
+  elemImg.setAttribute('src', projectImage)
+  elemImg.setAttribute('alt', 'Imagem de capa do projeto ' + projectName)
 
-    // Get Images
-    const elementPicture = document.createElement('picture')
-    const elementImg = document.createElement('img')
+  elemPicture.appendChild(elemImg)
 
-    elementImg.setAttribute('src', project.image)
+  return elemPicture
+}
 
-    elementPicture.appendChild(elementImg)
+const createStrong = (projectName) => {
+  const elemStrong = document.createElement('strong')
+  elemStrong.innerText = projectName
 
-    // Get HTML tags
-    const elementStrong = document.createElement('strong')
-    elementStrong.innerText = project.name
+  return elemStrong
+}
 
-    const elementTags = document.createElement('div')
-    project.tags.forEach(tag => {
-      const elementTag = document.createElement('span')
-      elementTag.innerText = tag
-    })
+const createTags = (projectTags) => {
+  const elemTags = document.createElement('div')
+
+  projectTags.forEach(tag => {
+    const elemTag = document.createElement('span')
+    elemTag.innerText = tag
+
+    elemTags.appendChild(elemTag)
   })
 
-  console.log(elementProjects)
+  return elemTags
+}
+
+const createProject = (project, index) => {
+  const elemProject = document.createElement('a')
+
+  elemProject.setAttribute('href', project.link)
+  elemProject.setAttribute('target', '_blank')
+
+  elemProject.setAttribute('data-aos', 'zoom-in-up')
+  elemProject.setAttribute('data-aos-duration', '800')
+  elemProject.setAttribute('data-aos-easing', 'ease-in-out')
+  elemProject.setAttribute('data-aos-offset', '-100')
+  elemProject.setAttribute('data-aos-delay', 300 * (index + 1))
+  
+  elemProject.classList.add('project')
+
+  // add imagem de capa
+  elemProject.appendChild(createImage(project.image, project.name))
+
+  // add nome do projeto
+  elemProject.appendChild(createStrong(project.name))
+
+  // add tags do projeto
+  elemProject.appendChild(createTags(project.tags))
+
+  return elemProject
+}
+
+const loadProjects = (projects) => {
+  projects.forEach((project, index) => {
+    elemProjects.appendChild(createProject(project, index))
+  });
 }
 
 fetch('./projects.json').then(response => response.json()).then(loadProjects)
